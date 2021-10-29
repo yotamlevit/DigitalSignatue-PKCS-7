dbd1119f51836e6647422076bdcff6eaaf7e9dbb
Yotam Levit <yotam.levit@gmail.com>
2021-10-29 19:02:53 +0300

    Addd serialization package

---
+++ serialization/__init__.py
@@ -0,0 +1,16 @@
+"""
+Date: 29/10/2021
+Aurthur: Yotam Levit
+Project - DigitalSignature
+"""
+
+from serialization.base_crypto import (
+    get_private_key,
+    get_certificate
+)
+
+
+__all__ = [
+    "get_private_key",
+    "get_certificate"
+]
---
+++ serialization/base_crypto.py
@@ -0,0 +1,34 @@
+"""
+Date: 29/10/2021
+Aurthur: Yotam Levit
+Project - DigitalSignature
+"""
+
+
+from cryptography import x509
+from cryptography.hazmat.primitives import serialization
+from utils import get_file_data_as_binary
+
+
+def get_private_key(path_to_private_key):
+    """
+    This function deserialize the private key
+    from a PEM format file to a pkey object
+    @param path_to_private_key: String - The path to the PEM format file
+                                         that contains the private key
+    @return: pkey object that contains the private key
+    """
+    private_key_buffer = get_file_data_as_binary(path_to_private_key)
+    return serialization.load_pem_private_key(private_key_buffer, None)
+
+
+def get_certificate(path_to_certificate):
+    """
+    This function deserialize the certificate
+    from a PEM format file to a x509 certificate object
+    @param path_to_certificate: String - The path to the PEM format file
+                                         that contains the certificate
+    @return: Cerfiticate object that contains the certificate
+    """
+    certificate_buffer = get_file_data_as_binary(path_to_certificate)
+    return x509.load_pem_x509_certificate(certificate_buffer)
