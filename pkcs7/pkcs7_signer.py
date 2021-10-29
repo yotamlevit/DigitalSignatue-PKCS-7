0aeff15a729a1232b46e51ebbff9e3f95d2cda12
Yotam Levit <yotam.levit@gmail.com>
2021-10-29 19:04:09 +0300

    Created pkcs7 class that implements pkcs7 signature

--- pkcs7/pkcs7_signer.py
+++ pkcs7/pkcs7_signer.py
@@ -4,47 +4,67 @@ Aurthur: Yotam Levit
 Project - DigitalSignature
 """
 
-from cryptography import x509
-from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.bindings.openssl.binding import Binding
 
-PATH_TO_PRIVATE_KEY = "./PKCS7-test-privkey.pem"
-PATH_TO_CERTIFICATE = "./PKCS7-test-sscert.pem"
-_lib = Binding.lib
-_ffi = Binding.ffi
 
+class PKCS7(Binding):
+    """
+    This Class is a child class to Binding from cryptography.
+    Its purpose is to implement PKCS7 Digital Signature and in particular
+    IBM`s DataPower Digital Signature format.
+    """
 
-def get_private_key(path_to_private_key):
-    with open(path_to_private_key, 'rb') as private_key_handler:
-        private_key_buffer = private_key_handler.read()
-    return serialization.load_pem_private_key(private_key_buffer, None)
+    def __init__(self, private_key=None, cert=None, data=None):
+        self.private_key = private_key
+        self.cert = cert
+        self.data = data
+        self._lib = super.lib
+        self._ffi = super.ffi
 
+    def __check_signature_parameters_existence(self):
+        """
+        This function checks if the needed parameters for signing exists
 
-def get_certificate(path_to_certificate):
-    with open(path_to_certificate, 'rb') as certificate_handler:
-        certificate_buffer = certificate_handler.read()
-    return x509.load_pem_x509_certificate(certificate_buffer)
+        ;return: True if all the parameters exists.
+                 False and the object if one of the parameters if None
+        """
+        return True if self.private_key and self.cert and self.data else False, self
 
+    def __validate_signature_parameters(self):
+        """
+        This function is the main validation function for parameters to digitally sign.
 
-def pkcs7_sign(data, private_key, cert):
-    bio_in = _lib.BIO_new_mem_buf(data.encode("utf-8"), len(data))
-    pkcs7_object = _lib.PKCS7_sign(cert._x509, private_key._evp_pkey, _ffi.NULL, bio_in, 0)
+        ;process: The function validates the following:
+                    - Existence of the parameters
+                    - Value of the parameters
 
-    bio_out = _lib.BIO_new(_lib.BIO_s_mem())
-    _lib.PEM_write_bio_PKCS7(bio_out, pkcs7_object)
+        ;return: True if all the condition for pkcs7 digital sign are right
+                 False if one of the condition for pkcs7 digital sign if not right (left... jk...)
+        """
+        if self.__check_signature_parameters_exist():
+            return True
+        return False
 
-    result_buffer = _ffi.new("char**")
-    buffer_length = _lib.BIO_get_mem_data(bio_out, result_buffer)
-    signed_data = _ffi.buffer(result_buffer[0], buffer_length)[:]
-    return signed_data.decode('utf-8')
+    def sign(self):
+        """
+        This function makes the pkcs7 signature it self
+        """
+        if self.__validate_signature_parameters():
+            bio_in = self._lib.BIO_new_mem_buf(self.data.encode("utf-8"), len(self.data))
+            pkcs7_object = self._lib.PKCS7_sign(self.cert._x509, self.private_key._evp_pkey, self._ffi.NULL, bio_in, 0)
+
+            bio_out = self._lib.BIO_new(self._lib.BIO_s_mem())
+            self._lib.PEM_write_bio_PKCS7(bio_out, pkcs7_object)
+
+            result_buffer = self._ffi.new("char**")
+            buffer_length = self._lib.BIO_get_mem_data(bio_out, result_buffer)
+            signed_data = self._ffi.buffer(result_buffer[0], buffer_length)[:]
+            return signed_data.decode('utf-8')
+        return False
 
 
 def main():
-    data = "test"
-    certificate = get_certificate(PATH_TO_CERTIFICATE)
-    private_key = get_private_key(PATH_TO_PRIVATE_KEY)
-    signed_data = pkcs7_sign(data, private_key, certificate)
-    print(signed_data)
+    pass
 
 if __name__ == '__main__':
     main()
