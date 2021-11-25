6a898f5c4b758d3d17ee30fbd4c9f22b2383ced2
yotamlevit <yotam.levit@gmail.com>
2021-11-25 23:07:39 +0200

    Better comments

--- serialization/base_crypto.py
+++ serialization/base_crypto.py
@@ -14,9 +14,12 @@ def get_private_key(path_to_private_key):
     """
     This function deserialize the private key
     from a PEM format file to a pkey object
+
     @param path_to_private_key: String - The path to the PEM format file
                                          that contains the private key
-    @return: pkey object that contains the private key
+
+    @return: The appropriate type of PrivateKey given an pkey data.
+            object that contains the private key.
     """
     private_key_buffer = get_file_data_as_binary(path_to_private_key)
     return serialization.load_pem_private_key(private_key_buffer, None)
@@ -26,9 +29,11 @@ def get_certificate(path_to_certificate):
     """
     This function deserialize the certificate
     from a PEM format file to a x509 certificate object
+
     @param path_to_certificate: String - The path to the PEM format file
                                          that contains the certificate
-    @return: Cerfiticate object that contains the certificate
+                                         
+    @return: Cerfiticate - object that contains the certificate
     """
     certificate_buffer = get_file_data_as_binary(path_to_certificate)
     return x509.load_pem_x509_certificate(certificate_buffer)
