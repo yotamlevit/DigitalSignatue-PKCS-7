0082303eb4499ef5f60b2317c1c22197036b8f6e
yotamlevit <yotam.levit@gmail.com>
2021-12-20 20:52:44 +0200

    Test the new Class

--- main.py
+++ main.py
@@ -3,10 +3,6 @@
 import utils
 import serialization
 from pkcs7 import PKCS7
-import time
-import gc
-import cryptography
-PKCS7_NOSIGS = 0x4  # defined in pkcs7.h
 
 def main():
 
@@ -14,9 +10,8 @@ def main():
     certificate = serialization.get_certificate("./data/PKCS7-test-sscert.pem")
     binary_data = utils.get_file_data_as_binary("./data/test.txt")
     pkcs7_signer = PKCS7(private_key, certificate, binary_data, "SHA256")
-    gc.enable()
     x = 0
-    while(x < 5):
+    while(x < 50000):
         x+=1
         singed_data = pkcs7_signer.no_detach_sign()
         print(x)
